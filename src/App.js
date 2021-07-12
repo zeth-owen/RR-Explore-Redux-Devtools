9f95466c4bbefe4eeb5cdb01adb6e257f45bc145
nickubed <nhpquandt@gmail.com>
2021-07-12 14:40:24 -0700

    just counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Counter from './Counter'
-import Todo from './Todo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Counter />
-      <Todo />
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
-import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
-        todos: todoReducer
+        counter: counterReducer
     }
 })
